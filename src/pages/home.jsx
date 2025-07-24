d24bc1299f32b49f1f39d848b838e91e063b468f
Meenakshi Sundaram A <meenakshisundaram.ams@gmail.com>
2025-07-24 16:57:17 +0530

    home UI/UX

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+
 import HomeBanner from "../component/home/banner";
 import HomeService from "../component/home/home-service";
 import HomeKeydiffer from "../component/home/home-keydiffer";
@@ -8,12 +8,14 @@ import Homeourexpertise from "../component/home/home-our-expertise";
 import Homeform from "../component/home/home-form";
 import Homenewsletter from "../component/home/home-newsletter";
 import Homecontact from "../component/home/home-contact";
+import HomeUIUX from "../component/home/home-ui-ux";
 
 function Home() {
   return (
     <>
       <HomeBanner />
       <HomeService />
+      <HomeUIUX/>
       <HomeKeydiffer />
       <Homewhychoose />
       <Hometestimonial />
