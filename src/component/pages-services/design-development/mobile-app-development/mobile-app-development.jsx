f8c45041fe412c2c5dd8721c998ba58c873c43f3
Sudeshcybomb <sudesh.t@CybombTech.onmicrosoft.com>
2025-07-30 08:05:10 +0530

    conflict change

--- src/component/pages-services/design-development/mobile-app-development/mobile-app-development.jsx
+++ src/component/pages-services/design-development/mobile-app-development/mobile-app-development.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './mobile-app-dev.module.css';
 import MobileAppDevContent from './mobile-app-dev-content';
-import MobileDevProcess from '../../../common-ui/agile-process/agile-process';
+import AgileProces from '../../../common-ui/agile-process/agile-process';
 import StillNotSure from '../../../common-ui/stillnotsure/StillNotSure';
 
 function MobileAppDevelopment() {
   return (
     <div className="mobile-app-dev-section">
       <MobileAppDevContent/>
-      
-      <MobileDevProcess/>
+
+      <AgileProces/>
       <StillNotSure/>
     </div>
   );
