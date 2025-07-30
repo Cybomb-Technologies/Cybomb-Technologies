ad3f9df66371abd43231e83febb1e26ebbbb12ed
Sudeshcybomb <sudesh.t@CybombTech.onmicrosoft.com>
2025-07-30 08:39:04 +0530

    form changes

--- src/component/banner-form/banner-form.jsx
+++ src/component/banner-form/banner-form.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './Bannerform.module.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import styles from "./Bannerform.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Bannerform() {
   return (
@@ -10,59 +10,62 @@ function Bannerform() {
           <form className={styles.requirementForm}>
             <div className="row">
               <div className="col-md-6 mb-4">
-                <input 
-                  type="text" 
-                  className={`form-control p-3 ${styles.formInput}`} 
-                  id="fullName" 
-                  placeholder='Full Name'
-                  required 
+                <input
+                  type="text"
+                  className={`form-control p-3 ${styles.formInput}`}
+                  id="fullName"
+                  placeholder="Full Name"
+                  required
                 />
               </div>
               <div className="col-md-6 mb-4">
-                <input 
-                  type="tel" 
-                  className={`form-control p-3 ${styles.formInput}`} 
-                  id="phoneNumber" 
-                  placeholder='Phone Number'
-                  required 
+                <input
+                  type="tel"
+                  className={`form-control p-3 ${styles.formInput}`}
+                  id="phoneNumber"
+                  placeholder="Phone Number"
+                  required
                 />
               </div>
             </div>
-            
-            <div className="mb-4">
-              <textarea 
-                className={`form-control p-3 ${styles.formInput}`} 
-                id="requirement" 
-                rows="3" 
-                placeholder='Your Requirement'
-                required
-              ></textarea>
-            </div>
-            
+
             <div className="row">
               <div className="col-md-6 mb-4">
-                <input 
-                  type="email" 
-                  className={`form-control p-3 ${styles.formInput}`} 
-                  id="email" 
-                  placeholder='Email'
-                  required 
+                <input
+                  type="email"
+                  className={`form-control p-3 ${styles.formInput}`}
+                  id="email"
+                  placeholder="Email"
+                  required
                 />
               </div>
 
               <div className="col-md-6 mb-4">
-                <input 
-                  type="text" 
-                  className={`form-control p-3 ${styles.formInput}`} 
-                  id="companyName" 
-                  placeholder='Company Name'
-                  required 
+                <input
+                  type="text"
+                  className={`form-control p-3 ${styles.formInput}`}
+                  id="companyName"
+                  placeholder="Company Name"
+                  required
                 />
               </div>
+
+              <div className="mb-4">
+                <textarea
+                  className={`form-control p-3 ${styles.formInput}`}
+                  id="requirement"
+                  rows="3"
+                  placeholder="Your Requirement"
+                  required
+                ></textarea>
+              </div>
             </div>
-            
+
             <div className="text-center">
-              <button type="submit" className={`btn ${styles.submitButton} w-100 bg-black text-white p-3`}>
+              <button
+                type="submit"
+                className={`btn ${styles.submitButton} w-100 bg-black text-white p-3`}
+              >
                 Submit Your Requirement
               </button>
             </div>
