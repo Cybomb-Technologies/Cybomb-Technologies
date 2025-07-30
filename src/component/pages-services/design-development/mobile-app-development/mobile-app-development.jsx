37187be563dc011ae267564832ab95556112d021
Sudeshcybomb <sudesh.t@CybombTech.onmicrosoft.com>
2025-07-30 08:10:42 +0530

    Resolved merge conflict in mobile-app-dev

--- src/component/pages-services/design-development/mobile-app-development/mobile-app-development.jsx
+++ src/component/pages-services/design-development/mobile-app-development/mobile-app-development.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './mobile-app-dev.module.css';
 import MobileAppDevContent from './mobile-app-dev-content';
-import AgileProces from '../../../common-ui/agile-process/agile-process';
+import AgileProcess from '../../../common-ui/agile-process/agile-process';
 import StillNotSure from '../../../common-ui/stillnotsure/StillNotSure';
 
 function MobileAppDevelopment() {
   return (
     <div className="mobile-app-dev-section">
-      <MobileAppDevContent/>
-
-      <AgileProces/>
-      <StillNotSure/>
+      <MobileAppDevContent />
+      <AgileProcess />
+      <StillNotSure />
     </div>
   );
 }
