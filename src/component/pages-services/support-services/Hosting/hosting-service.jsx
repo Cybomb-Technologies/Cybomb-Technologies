0261845b764b126ce15c07dfebe3640e35523a42
DivyaBaskaran0511 <divyabaskaranopm@gmail.com>
2025-08-01 14:10:40 +0530

    added commom ui for hosting service

--- src/component/pages-services/support-services/Hosting/hosting-service.jsx
+++ src/component/pages-services/support-services/Hosting/hosting-service.jsx
@@ -1,14 +1,13 @@
 
 
 import HostingServicesContent from './hosting-service-content';
-import AgileProcess from '../../../common-ui/agile-process/agile-process';
 import StillNotSure from '../../../common-ui/stillnotsure/StillNotSure';
 
 function HostingServices() {
   return (
     <>
       <HostingServicesContent/>
-      <AgileProcess/>
+      
       <StillNotSure/>
     </>
   );
