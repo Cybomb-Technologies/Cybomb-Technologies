0350681f76fac20843c27430de02bb90eaf7dc18
dharshinicybomb <divyadharshini@cybomb.com>
2025-08-06 14:06:35 +0530

    wordpress

---
+++ src/component/pages-services/technologies/cms/wordpress/wordpress.jsx
@@ -0,0 +1,13 @@
+import AgileProcess from "../../../../common-ui/agile-process/agile-process";
+import WordPressContent from "./wordpress-content";
+
+function WordPress() {
+  return (
+    <>
+      <WordPressContent />
+      <AgileProcess />
+    </>
+  );
+}
+
+export default WordPress;
