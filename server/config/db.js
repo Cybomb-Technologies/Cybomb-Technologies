53e31b27afd2a8825ff30c37d83bb805acce46db
Sudeshcybomb <sudesh.t@CybombTech.onmicrosoft.com>
2025-11-05 17:50:50 +0530

    Sudesh

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(
+      "mongodb://sudesh.t%40cybomb.com:Cybomb%401234@147.93.111.96:27017/cybomb?authSource=admin"
+    );
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
